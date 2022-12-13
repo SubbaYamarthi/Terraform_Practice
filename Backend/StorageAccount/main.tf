179318dbff6ca9eab9ab811b58595980ac28d8b9
SubbaYamarthi <116677540+SubbaYamarthi@users.noreply.github.com>
2022-12-13 17:36:17 +0530

    Add files via upload

---
+++ Backend/StorageAccount/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "RG-tfstate"
+        storage_account_name = "strtfstatesubba"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "random_string" "random" {
+    length = 6
+    special = false
+    upper = false
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "${lower(var.base_name)}${random_string.random.result}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
---
+++ Backend/StorageAccount/terraform.tfvars
@@ -0,0 +1,3 @@
+base_name = "Rgdemo"
+location = "East US"
+resource_group_name = "RGdemo2"
