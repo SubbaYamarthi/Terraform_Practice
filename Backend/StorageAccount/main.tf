bfe096b1f34a254646536034a87833ffa2c06b17
SubbaYamarthi <116677540+SubbaYamarthi@users.noreply.github.com>
2022-12-13 17:38:47 +0530

    Update main.tf

--- Backend/StorageAccount/main.tf
+++ Backend/StorageAccount/main.tf
@@ -1,22 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-    backend "azurerm" {
-        resource_group_name  = "RG-tfstate"
-        storage_account_name = "strtfstatesubba"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
 
-}
-
-provider "azurerm" {
-  features {}
-}
 
 
 resource "random_string" "random" {
